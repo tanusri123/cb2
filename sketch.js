de23509af1ebe78e6ff167094c888f9935246e0f
tanusri123 <67907113+tanusri123@users.noreply.github.com>
2020-08-11 19:42:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var dustbin,paper
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+(ground = BODIES.rectangle(200,380,400,20,option))
+(dustbin=createSprite(320,200,50,20))
+(paper=createSprite(150,200,10,10))
+
+	
+
+
+function setup() {
+	createCanvas(400, 400);
+
+    ellipseMode(RADIUS)
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	dustbin=createSprite(320,400,50,20);
+	dustbin.shapeColor = (0,225,0)
+    ground = BODIES.rectangle(200,380,400,20,option);
+	paper=createSprite(150,390,10,10);
+	Matter.Body.circle(320,200,0.5,option2,right);
+	WORLD.add(world,ground)
+	paperObject.radius=70;
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  function keyPressed() {
+	if (keyCode === UP_ARROW) {
+	   // Look at the hints in the document and understand how to make the package body fall only on
+	   Matter.Body.setStatic={body};
+	   isStatic=false;
+	   Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x=85,y=-85} )
+	   paper.velocityX=4;
+	   paper.velocityY=5;
+	   var options={
+	   restitution=0.5,
+	   friction=0.5,
+	   density=1.2}
+
+	   display()
+	   paperObject.dislay();
+	   dustbinObject.display();
+	   groundObject.display();
+	
+	   
+	   drawSprites();
+	}}}
